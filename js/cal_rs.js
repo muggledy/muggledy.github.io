db3ef0fa6f2b37583406033924127a9449e15c5d
muggledy <3101266674@qq.com>
2021-07-12 20:58:35 +0800

    Site updated: 2021-07-12 20:58:27

--- js/cal_rs.js
+++ js/cal_rs.js
@@ -1,13 +1,1 @@
-/*本脚本是为git calendar编写的，页面右下角存在三个能导致calendar所在容器大小发生改变的按钮，原本的机制是当窗口大小发生改变(onresize事件)时重绘calendar，于是现在当这几个按钮被按下时手动触发onresize事件*/
-var dyhiddenbutton=document.getElementById("hide-aside-btn");
-
-if (dyhiddenbutton) {
-	dyhiddenbutton.onclick=function(){
-        if (document.getElementById("new-posts-chart")||document.getElementById("new-tags-chart")||document.getElementById("new-categories-chart")){
-            dyasidebtn = true; //服务于t_chart.js
-            dydoresize(0); //目前为了用于dyasidebtn，特地将时延置为了0 因为改为了连续resize
-        }else{
-            dydoresize(0); //这是为了首页的history_calendar服务的
-        }
-    };
-}
+"use strict";var dyhiddenbutton=document.getElementById("hide-aside-btn");dyhiddenbutton&&(dyhiddenbutton.onclick=function(){(document.getElementById("new-posts-chart")||document.getElementById("new-tags-chart")||document.getElementById("new-categories-chart"))&&(dyasidebtn=!0),dydoresize(0)});
