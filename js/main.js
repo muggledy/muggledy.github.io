843701201b3282f2805d4bad6bb6020b72ae4292
muggledy <zgjsycfndy2015@163.com>
2023-05-23 01:44:54 +0800

    GitHub Actions Auto Builder at 2023-05-23 01:44:54

--- js/main.js
+++ js/main.js
@@ -1,3 +1,72 @@
+var support_v2 = {
+  searchParams: 'URLSearchParams' in self,     //queryString处理函数
+  iterable: 'Symbol' in self && 'iterator' in Symbol,
+  blob:
+    'FileReader' in self &&
+    'Blob' in self &&
+    (function() {
+      try {
+        new Blob()
+        return true
+      } catch (e) {
+        return false
+      }
+    })(),
+  formData: 'FormData' in self,
+  arrayBuffer: 'ArrayBuffer' in self       //二进制存储
+}
+function parseHeaders_v2(rawHeaders) {
+  var headers = new Headers()
+  // Replace instances of \r\n and \n followed by at least one space or horizontal tab with a space
+  // https://tools.ietf.org/html/rfc7230#section-3.2
+  var preProcessedHeaders = rawHeaders.replace(/\r?\n[\t ]+/g, ' ')
+  preProcessedHeaders.split(/\r?\n/).forEach(function(line) {
+    var parts = line.split(':')
+    var key = parts.shift().trim()
+    if (key) {
+      var value = parts.join(':').trim()
+      headers.append(key, value)
+    }
+  })
+  return headers
+}
+function fetch_v2(input, init) {
+  return new Promise(function(resolve, reject) {
+    var request = new Request(input, init)
+    var xhr = new XMLHttpRequest()
+    xhr.onload = function() {
+      var options = {
+        status: xhr.status,
+        statusText: xhr.statusText,
+        headers: parseHeaders_v2(xhr.getAllResponseHeaders() || '')
+      }
+      options.url = 'responseURL' in xhr ? xhr.responseURL : options.headers.get('X-Request-URL')
+      var body = 'response' in xhr ? xhr.response : xhr.responseText
+      resolve(new Response(body, options))
+    }
+    xhr.onerror = function() {
+      reject(new TypeError('Network request failed'))
+    }
+    xhr.ontimeout = function() {
+      reject(new TypeError('Network request failed'))
+    }
+    xhr.open(request.method, request.url, true)
+    if (request.credentials === 'include') {
+      xhr.withCredentials = true
+    }
+    if ('responseType' in xhr && support_v2.blob) {
+      xhr.responseType = 'blob'
+    }
+    request.headers.forEach(function(value, name) {
+      xhr.setRequestHeader(name, value)
+    })
+    //设置fetch函数请求的超时时间
+    if(init!=null&&init.timeout!=null){
+      xhr.timeout=init.timeout;
+    }
+    xhr.send(typeof request._bodyInit === 'undefined' ? null : request._bodyInit)
+  })
+}
 document.addEventListener('DOMContentLoaded', function () {
   const $blogName = document.getElementById('site-name')
   let blogNameWidth = $blogName && $blogName.offsetWidth
