be7646f90981b3eedc5937dd16c194d44d15f71d
muggledy <zgjsycfndy2015@163.com>
2024-06-23 11:15:13 +0800

    GitHub Actions Auto Builder at 2024-06-23 11:15:13

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'MuggledyBlogCacheHelper';
 let cachelist = ["/404.html"];
 
-const blog_default_version = '1.0.30'
+const blog_default_version = '1.0.31'
 
 self.cons = {
     s: (m) => {
